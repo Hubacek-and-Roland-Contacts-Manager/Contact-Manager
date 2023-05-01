0c8baadc455ca8e8a7d3600855167d4e36d72c24
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-05-01 12:05:43 -0500

    Added contact

---
+++ src/contact.java
@@ -0,0 +1,17 @@
+package src;
+
+public class contact {
+    private String name;
+    private String number;
+
+    public contact(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+public String getName(){
+        return this.name;
+}
+public String getNumber(){
+        return this.number;
+}
+}
