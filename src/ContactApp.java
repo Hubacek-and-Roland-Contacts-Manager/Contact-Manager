cd6fc3862bf1971d7c21f45e172aa960613ce87a
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-05-02 11:33:33 -0500

    will come back

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -11,41 +11,43 @@ public class ContactApp {
 
     public static void main(String[] args) {
 // to run all the apps together
-        runApp();
         validateFile();
         loadContacts();
+        runApp();
 
     }
-public static void runApp(){
+    public static void runApp() {
         boolean running = true;
 
-        while (running){
+        while (running) {
             System.out.println("1. View contacts.\n2. Add a new contact.\n3. Search a contact by name.\n4. Delete an existing contact.\n5. Exit.\nEnter an option (1, 2, 3, 4 or 5):\n");
-//        Function for call which options
+
+            // Add the switch statement here
             int Answer = scanner.nextInt();
             scanner.nextLine();
-                            case 1 -> AllContacts();
-                            case 2 -> addContacts();
-                            case 3 -> searchContact();
-                            case 4 -> deleteContact();
-            if (Answer == 5) {
-                running = false;
-            } else {
-                System.out.println("Please enter a valid menu option");
+            switch (Answer) {
+                case 1 -> AllContacts();
+                case 2 -> addContacts();
+                case 3 -> searchContact();
+                case 4 -> deleteContact();
+                default -> {
+                    if (Answer == 5) {
+                        running = false;
+                    } else {
+                        System.out.println("Please enter a valid menu option");
+                    }
+
+                }
+            }if (running) {
+                System.out.println("continue...? (y/n)");
+                if (scanner.nextLine().equalsIgnoreCase("n")) {
+                    running = false;
+                }
             }
-        }
-//        Add a continue if user is done or not
-    if (running) {
-        System.out.println("%continue...?");
-        if (scanner.nextLine().equalsIgnoreCase("n")){
-            running = false;
+            if (!running) System.out.println("Have a nice day!");
         }
     }
 
-    if(running) System.out.println("Have a nice day!");
-
-
-}
 
     public  static String formatNumber(String num) {
         String fnumber = num;
@@ -65,10 +67,19 @@ public static void runApp(){
         System.out.println("Enter a name of contact to delete.");
         String isName = scanner.nextLine();
         for (contact c : Contacts){
-            toRemove = c;
+            if (c.getName().equalsIgnoreCase(isName)){
+                toRemove = c;
+            }
         }
-    }
+        if (toRemove == null){
+            System.out.println("invalid name");
+        } else {
+            Contacts.remove(toRemove);
+            System.out.println("contact deleted");
+            saveContacts();
+        }
 
+    }
 
     public static void searchContact(){
         boolean isSuccessful = false;
@@ -83,7 +94,7 @@ public static void runApp(){
         }
         if (!isSuccessful) System.out.println("Invalid name");
     }
-    public static void displayContacts(){
+    public static void AllContacts(){
         if (Contacts.size() >= 1)contactFormatter();
         else System.out.println("No Contacts found....");
     }
@@ -181,7 +192,7 @@ public static void runApp(){
         }
         if(passable) {
             contact c = new contact(outName, outNumber);
-            System.out.printf("%sContact for %s added!%s%n", outName);
+            System.out.println("Contact for added!");
             Contacts.add(c);
             saveContacts();
         }
@@ -192,7 +203,8 @@ public static void runApp(){
         System.out.printf("│%-19s│%-19s│%n", "Name","Phone Number");
         System.out.println("┝━━━━━━━━━━━━━━━━━━━┿━━━━━━━━━━━━━━━━━━━┥");
         for (contact c : Contacts) {
-            System.out.println("│%s%-19s%s│%s%-19s%s│%n");
+            System.out.printf("│%s%-19s%s│%s%-19s%s│%n", "| ", c.getName(), " |", "| ", formatNumber(c.getNumber()), " |");
+
         }
         System.out.println("└───────────────────┴───────────────────┘");
     }
